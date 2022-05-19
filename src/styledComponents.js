e911d421aeeb058a3dac37a05fc1e3bd07af4e74
Denis <example@gmail.com>
2022-05-19 12:23:17 +0300

    add delete and show button functions

--- src/styledComponents.js
+++ src/styledComponents.js
@@ -7,9 +7,29 @@ export const Wrapper = styled.div`
 
 export const AddHeightButton = styled.div`
 	position: absolute;
-    bottom: -50px;
-    left: 2px;
+	bottom: -50px;
+	left: 2px;
 	background-color: orange;
+	:before {
+        content: '';
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        width: 14px;
+        height: 4px;
+        background-color: #fff;
+	}
+	:after {
+		content: '';
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        width: 4px;
+        height: 14px;
+        background-color: #fff;
+	}
 	${props => `width: ${props.cellSize + 'px'};
 				height: ${props.cellSize + 'px'};
 				bottom: ${'-' + props.cellSize + 'px'};
@@ -20,32 +40,73 @@ export const AddWidthButton = styled.div`
 	position: absolute;
     top: 2px;
 	background-color: orange;
+	:before {
+        content: '';
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        width: 14px;
+        height: 4px;
+        background-color: #fff;
+	}
+	:after {
+		content: '';
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        width: 4px;
+        height: 14px;
+        background-color: #fff;
+	}
 	${props => `width: ${props.cellSize + 'px'};
 				height: ${props.cellSize + 'px'};
 				right: ${"-" + props.cellSize + 'px'};
 	`};
 `
 
-export const RemoveHeightButton = styled.div`
+export const DeleteRowButton = styled.div`
 	position: absolute;
 	background-color: orangered;
 	transition: all .3s;
-	${props => `width: ${props.cellSize + 'px'};
-				height: ${props.cellSize + 'px'};
-				top: ${"-" + props.cellSize + 'px'};
-				left: ${props.offsetLeft + 'px'};
-				display: ${props.buttonsDisplay ? 'block' : 'none'};
-	`};
-`
-
-export const RemoveWidthButton = styled.div`
-	position: absolute;
-	background-color: orangered;
-	transition: all .3s;
+	:before {
+        content: '';
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        width: 14px;
+        height: 4px;
+        background-color: #fff;
+	}
 	${props => `width: ${props.cellSize + 'px'};
 				height: ${props.cellSize + 'px'};
 				left: ${"-" + props.cellSize + 'px'};
 				top: ${props.offsetTop + 'px'};
+				display: ${props.buttonsDisplay ? 'block' : 'none'};
+	`};
+`
+
+
+export const DeleteColumnButton = styled.div`
+	position: absolute;
+	background-color: orangered;
+	transition: all .3s;
+	:before {
+        content: '';
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        width: 14px;
+        height: 4px;
+        background-color: #fff;
+	}
+	${props => `width: ${props.cellSize + 'px'};
+				height: ${props.cellSize + 'px'};
+				top: ${"-" + props.cellSize + 'px'};
+				left: ${props.offsetLeft + 'px'};
 				display: ${props.buttonsDisplay ? 'block' : 'none'};
 	`};
 `
